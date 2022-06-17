3554d95a609ace6081d34e5038bdd7b9bbe62b1d
maniart <mani.art@gmail.com>
2022-06-17 09:29:12 -0400

    fix nested installation

--- config/database.js
+++ config/database.js
@@ -1,13 +1,32 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+const path = require('path');
+
+module.exports = ({ env }) => {
+  // if (env('NODE_ENV') === 'production') {
+  //   return {
+  //     connection: {
+  //       client: 'postgres',
+  //       connection: {
+  //         host: env('DATABASE_HOST', '127.0.0.1'),
+  //         port: env.int('DATABASE_PORT', 5432),
+  //         database: env('DATABASE_NAME', 'strapi'),
+  //         user: env('DATABASE_USERNAME', 'strapi'),
+  //         password: env('DATABASE_PASSWORD', 'strapi'),
+  //         ssl: {
+  //           rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+  //         },
+  //       },
+  //       debug: false,
+  //     },
+  //   };
+  // }
+
+  return {
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'prefercity-backend'),
-      user: env('DATABASE_USERNAME', 'prefercity'),
-      password: env('DATABASE_PASSWORD', 'prefercity'),
-      ssl: env.bool('DATABASE_SSL', true),
-    },
-  },
-});
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
+    }
+  }  
+};
