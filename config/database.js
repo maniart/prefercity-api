e72db760e34f0fd6a68d791df5444037a1f9a047
maniart <mani.art@gmail.com>
2022-06-20 17:28:38 -0400

    Update db setting

--- config/database.js
+++ config/database.js
@@ -1,24 +1,6 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-  // if (env('NODE_ENV') === 'production') {
-  //   return {
-  //     connection: {
-  //       client: 'postgres',
-  //       connection: {
-  //         host: env('DATABASE_HOST', '127.0.0.1'),
-  //         port: env.int('DATABASE_PORT', 5432),
-  //         database: env('DATABASE_NAME', 'strapi'),
-  //         user: env('DATABASE_USERNAME', 'strapi'),
-  //         password: env('DATABASE_PASSWORD', 'strapi'),
-  //         ssl: {
-  //           rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-  //         },
-  //       },
-  //       debug: false,
-  //     },
-  //   };
-  // }
 
   return {
     connection: {
